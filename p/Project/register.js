1f7e11cc981a2e13f5989ad1a647b357aafde5c0
09amitpatil <patil09amit@gmail.com>
2024-04-05 21:40:15 +0530

    update

--- p/Project/register.js
+++ p/Project/register.js
@@ -1,26 +1,26 @@
 document.getElementById("btnp").addEventListener("click", sub)
 
 function sub() {
-    var a1 = document.getElementById("validationCustom01").value;
-    var a2 = document.getElementById("validationCustom02").value;
-    var a3 = document.getElementById("validationCustom03").value;
-    var a4 = document.getElementById("validationCustom04").value;
+    var a1 = document.getElementById("Input1").value;
+    var a2 = document.getElementById("Input2").value;
+    var a3 = document.getElementById("Input3").value;
 
-    if (a1 == "" && a2 == "" && a3 == "" && a4 == "") {
+    if (a1 == "" && a2 == "" && a3 == "") {
         document.getElementById("place1").style.color = "red";
         document.getElementById("place2").style.color = "red";
         document.getElementById("place1").innerHTML = "Error!!";
         document.getElementById("place2").innerHTML = "Fill all required details!!";
+    }
+    else if(a2!=a3){
+        document.getElementById("place1").style.color = "red";
+        document.getElementById("place1").innerHTML = "Error password dose not match!!";
 
     }
-    else if (a1 !== "" && a2 !== "" && a3 !== "" && a4 !== "") {
+    else if (a1 !== "" && a2 !== "" && a3 !== "" ) {
         document.getElementById("place1").style.color = "green";
         document.getElementById("place2").style.color = "green";
         document.getElementById("place1").innerHTML = "SUCCESS!!";
-        document.getElementById("place2").innerHTML = "Hurray you are a seller now!!";
+        document.getElementById("place2").innerHTML = "You are registered proceed to login!!";
 
     }
-
-
-
 }
