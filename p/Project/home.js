c2dbea2ee7e921fd7688732088cd82ec7e75ea99
09amitpatil <patil09amit@gmail.com>
2024-04-06 13:49:53 +0530

    update

--- p/Project/home.js
+++ p/Project/home.js
@@ -1,26 +1,26 @@
 document.getElementById("btnp").addEventListener("click", sub)
 
-function sub() {
+function sub() 
+{
     var a1 = document.getElementById("validationCustom01").value;
     var a2 = document.getElementById("validationCustom02").value;
     var a3 = document.getElementById("validationCustom03").value;
     var a4 = document.getElementById("validationCustom04").value;
 
-    if (a1 == "" && a2 == "" && a3 == "" && a4 == "") {
+    if (a1 == "" && a2 == "" && a3 == "" && a4 == "") 
+    {
         document.getElementById("place1").style.color = "red";
         document.getElementById("place2").style.color = "red";
         document.getElementById("place1").innerHTML = "Error!!";
         document.getElementById("place2").innerHTML = "Fill all required details!!";
 
     }
-    else if (a1 !== "" && a2 !== "" && a3 !== "" && a4 !== "") {
+    else if (a1 != "" && a2 != "" && a3 != "" && a4 != "") 
+    {
         document.getElementById("place1").style.color = "green";
         document.getElementById("place2").style.color = "green";
         document.getElementById("place1").innerHTML = "SUCCESS!!";
         document.getElementById("place2").innerHTML = "Hurray you are a seller now!!";
 
     }
-
-
-
 }
--- p/Project/register.js
+++ p/Project/register.js
@@ -1,26 +1,27 @@
-document.getElementById("btnp").addEventListener("click", sub)
-
-function sub() {
+document.getElementById("btnp").addEventListener("click", function(event) {
+    event.preventDefault(); 
     var a1 = document.getElementById("Input1").value;
     var a2 = document.getElementById("Input2").value;
     var a3 = document.getElementById("Input3").value;
+    var place1 = document.getElementById("place1");
+    var place2 = document.getElementById("place2");
 
-    if (a1 == "" && a2 == "" && a3 == "") {
-        document.getElementById("place1").style.color = "red";
-        document.getElementById("place2").style.color = "red";
-        document.getElementById("place1").innerHTML = "Error!!";
-        document.getElementById("place2").innerHTML = "Fill all required details!!";
-    }
-    else if(a2!=a3){
-        document.getElementById("place1").style.color = "red";
-        document.getElementById("place1").innerHTML = "Error password dose not match!!";
+    if (a1 === "" || a2 === "" || a3 === "") {
+        place1.style.color = "red";
+        place2.style.color = "red";
+        place1.innerHTML = "Error!!";
+        place2.innerHTML = "Fill all required details!!";
+    } else if (a2 !== a3) {
+        place1.style.color = "red";
+        place1.innerHTML = "Error password dose not match!!";
+    } else {
+        place1.style.color = "green";
+        place2.style.color = "green";
+        place1.innerHTML = "SUCCESS!!";
+        place2.innerHTML = "You are registered you will now redirected to login page!!";
 
+        setTimeout(function() {
+            window.location.href = "login.html";
+        }, 2000); 
     }
-    else if (a1 !== "" && a2 !== "" && a3 !== "" ) {
-        document.getElementById("place1").style.color = "green";
-        document.getElementById("place2").style.color = "green";
-        document.getElementById("place1").innerHTML = "SUCCESS!!";
-        document.getElementById("place2").innerHTML = "You are registered proceed to login!!";
-
-    }
-}
+});
